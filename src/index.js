5511c3857633566ca7fd97baaf7e85df94fdaaa6
michelzalotti <michelzalotti@gmail.com>
2024-04-29 13:13:10 -0300

    Inicio do projeto e configurações iniciais.

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const characters = [
+  {
+    nome: "Mario",
+    velocidade: 4,
+    manobrabilidade: 3,
+    poder: 3,
+  },
+  {
+    nome: "Princesa",
+    velocidade: 3,
+    manobrabilidade: 4,
+    poder: 2,
+  },
+  {
+    nome: "Yoshi",
+    velocidade: 2,
+    manobrabilidade: 4,
+    poder: 3,
+  },
+  {
+    nome: "Bowser",
+    velocidade: 5,
+    manobrabilidade: 2,
+    poder: 5,
+  },
+  {
+    nome: "Luigi",
+    velocidade: 3,
+    manobrabilidade: 4,
+    poder: 4,
+  },
+  {
+    nome: "Donkey Kong",
+    velocidade: 2,
+    manobrabilidade: 2,
+    poder: 5,
+  },
+];
+
+const blocks = ["Reta", "Curva", "Confronto"];
